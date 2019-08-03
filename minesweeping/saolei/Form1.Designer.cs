85950995d3d076959632c498db5245da62cc0ac4
Jelech <jelech@hotmail.com>
2019-08-04 00:09:42 +0800

    game upgrade: minesweeping
    c# winform minesweeping :扫雷 cheatCode dont need

---
+++ minesweeping/saolei/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace shiyan1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 676);
+            this.Name = "Form1";
+            this.Text = "扫雷";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.label1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ minesweeping/saolei/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace shiyan1
+{
+    public partial class Form1 : Form
+    {
+        private int[,] graphBomb = new int[22, 22];
+        private int[,] bombs = new int[22, 22];
+        private int bombNum = 0;
+        private const int EDGE = 32;
+        private int flagNum = 0;
+        private List<Tuple<int, int>> flags;
+        public Form1()
+        {
+            InitializeComponent();
+            InitBomb();
+            //DrawBomb();
+
+        }
+
+        private void InitBomb()
+        {
+            int[,] dir = new int[2, 9] { { -1,-1,-1,0,0,0,1,1,1 },{ -1,0,1,-1,0,1,-1,0,1 } };
+            Random random = new Random(1);
+            bombNum = 50;
+            flagNum = bombNum;
+            flags = new List<Tuple<int, int>>(bombNum);
+
+            while (bombNum > 0)
+            {
+
+                int x = random.Next(20);
+                int y = random.Next(20);
+                if (bombs[x, y] != 0) continue;
+                bombs[x, y] = 1;
+                bombNum--;
+                for (int k = 0; k < 9; k++)
+                {
+                    if (x + dir[0, k] < 0 || x + dir[0, k] >= 20 || y + dir[1, k] < 0 || y + dir[1, k] >= 20)
+                        continue;
+                    graphBomb[x + dir[0, k], y+dir[1,k]] += 1;
+                }
+
+                //Console.WriteLine(x.ToString() +" "+y.ToString());
+            }
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    if (bombs[i, j] > 0) graphBomb[i, j] = -1;
+                    
+                    //Console.Write(graphBomb[i,j]+"\t");
+                }
+                //Console.WriteLine();
+            }
+        }
+
+        private void DrawBomb()
+        {
+            Graphics g = this.CreateGraphics();
+            Pen mypen = new Pen(Color.Black, 2);
+            Brush mybrush = new SolidBrush(Color.White);
+
+            const int EDGE = 21;
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    g.DrawRectangle(mypen ,i * EDGE, j * EDGE, EDGE - 1, EDGE - 1);
+                }
+            }
+            
+
+
+        }
+        
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("mousedown:" + e.Location);
+            int nx = e.X/EDGE;
+            int ny = e.Y/EDGE;
+            //Console.WriteLine(nx.ToString() + " " + ny.ToString());
+            if(e.Button == MouseButtons.Right)
+            {
+                g.DrawString("X", font, Brushes.Black, nx * EDGE, ny * EDGE);
+                flags.Add(new Tuple<int, int>(nx, ny));
+                flagNum--;
+                if(flagNum == 0)
+                {
+                    foreach(var x in flags)
+                    {
+                        if(bombs[x.Item1,x.Item2] <= 0)
+                        {
+                            return;
+                        }
+                    }
+                    MessageBox.Show("you win");
+                }
+                return;
+            }
+            Brush mybrush3 = new SolidBrush(Color.Red);
+            if (bombs[nx,ny] > 0)
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    for (int j = 0; j < 20; j++)
+                    {
+                        if (bombs[i, j] > 0) g.FillRectangle(mybrush3, i * EDGE, j * EDGE, EDGE - 2, EDGE - 2);
+                    }
+                }
+                MessageBox.Show("GAME OVER");
+            }
+
+            // can swap
+            int[,] dir = new int[2, 8] { { -1, -1, -1, 0, 0, 1, 1, 1 }, { -1, 0, 1, -1, 1, -1, 0, 1 } };
+            Queue<Tuple<int,int>> Q = new Queue<Tuple<int, int>>(500);
+            Q.Enqueue(new Tuple<int, int>(nx,ny));
+            while (Q.Count > 0)
+            {
+                Tuple<int, int> temp = Q.Dequeue();
+                int x = temp.Item1, y = temp.Item2;
+                bombs[x, y] = -1;
+                //Console.WriteLine(x.ToString()+"_"+y.ToString());
+                g.FillRectangle(mybrush2, x * EDGE, y * EDGE, EDGE - 2, EDGE - 2);
+                g.DrawString(graphBomb[x, y].ToString(), font, Brushes.Black, x * EDGE, y * EDGE);
+                if(graphBomb[x,y] != 0)
+                {
+                    continue;
+                }
+
+                // is zero
+                for (int k = 0; k < 8; k++)
+                {
+                    if (x + dir[0, k] < 0 || x + dir[0, k] >= 20 || y + dir[1, k] < 0 || y + dir[1, k] >= 20 || bombs[x + dir[0, k], y + dir[1, k]] == -1)
+                        continue;
+                    bombs[x + dir[0, k], y + dir[1, k]] = -1;
+                    Q.Enqueue(new Tuple<int, int>(x + dir[0, k], y + dir[1, k]));
+
+                }
+            }
+
+        }
+
+        private Graphics g;
+        private Pen mypen;
+        private Brush mybrush;
+        private Brush mybrush2;
+        private Font font = new Font("华为宋体", 16);
+        private void label1_Paint(object sender, PaintEventArgs e)
+        {
+            if(this.g==null) this.g = this.CreateGraphics();
+            if(this.mypen == null) this.mypen = new Pen(Color.Black, 2);
+            if(this.mybrush == null) this.mybrush = new SolidBrush(Color.White);
+            if(this.mybrush2 == null) this.mybrush2 = new SolidBrush(Color.Gray);
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    g.FillRectangle(mybrush, i * EDGE, j * EDGE, EDGE - 2, EDGE - 2);
+                    //g.DrawString(graphBomb[i, j].ToString(), font, Brushes.Red, i * EDGE, j * EDGE);
+                }
+            }
+        }
+    }
+}
